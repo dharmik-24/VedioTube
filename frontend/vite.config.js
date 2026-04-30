3a6e4fc812c58d4401dc9d6d383ae578b0fc1184
Dharmik <dharmikkansara24@gmail.com>
2026-04-30 10:28:59 +0530

    Backend URL change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://vediotube-8x90.onrender.com',
         changeOrigin: true,
       }
     }
